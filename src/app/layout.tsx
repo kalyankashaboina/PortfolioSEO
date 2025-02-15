bb2ca9bb453b5a72db1ecbc1346ca87963f62eed
kalyankashaboina <kalyankashaboina07@gmail.com>
2025-02-15 14:33:23 +0530

    setteled the urls

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,18 +17,40 @@ export const metadata: Metadata = {
   title: "Kalyan Kashaboina - Web Developer & Portfolio",
   description: "Kalyan Kashaboina's Web Developer Portfolio showcasing front-end projects and skills.",
   keywords: "Kalyan Kashaboina, Kalyan Kashaboina Web Developer, Kalyan Kashaboina Portfolio, Kalyan Kashaboina Frontend Developer",
+  openGraph: {
+    title: "Kalyan Kashaboina - Web Developer & Portfolio",
+    description: "Kalyan Kashaboina's Web Developer Portfolio showcasing front-end projects and skills.",
+    url: "https://portfolio-git-master-kalyankashaboinas-projects.vercel.app/",
+    type: "website",
+    siteName: "Kalyan Kashaboina - Web Developer", // Site name for Open Graph
+    images: [
+      {
+        url: "https://portfolio-git-master-kalyankashaboinas-projects.vercel.app/images/kalyan1.jpg", // Replace with an actual image
+        width: 800,
+        height: 600,
+        alt: "Kalyan Kashaboina Portfolio Image",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <html lang="en">
-        <head>
+    <html lang="en">
+      <head>
         <meta name="google-site-verification" content="I38EL_WVPO6tcA1cmsmLFvAZ9Szco0VLvjph8OxIFcA" />
-        </head>
-        <body>
-          {children}
-          <Footer />
-        </body>
-      </html>
+        <meta property="og:title" content="Kalyan Kashaboina - Web Developer & Portfolio" />
+        <meta property="og:description" content="Kalyan Kashaboina's Web Developer Portfolio showcasing front-end projects and skills." />
+        <meta property="og:url" content="https://portfolio-git-master-kalyankashaboinas-projects.vercel.app/" />
+        <meta property="og:site_name" content="Kalyan Kashaboina - Web Developer" />
+        <meta property="og:image" content="https://portfolio-git-master-kalyankashaboinas-projects.vercel.app/images/kalyan1.jpg" /> {/* Replace with actual image URL */}
+        <meta property="og:image:width" content="800" />
+        <meta property="og:image:height" content="600" />
+      </head>
+      <body>
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
